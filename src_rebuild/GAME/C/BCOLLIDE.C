1ed78a5a473c2f4e2eae15418f4400ecbb640bcc
Ilya Shurumov <ishurumov@gmail.com>
2020-05-10 13:49:22 +0600

    - implement DamageCar3D

--- src_rebuild/GAME/C/BCOLLIDE.C
+++ src_rebuild/GAME/C/BCOLLIDE.C
@@ -768,11 +768,9 @@ LAB_0001cf9c:
 	/* end block 3 */
 	// End Line: 1169
 
+// [D]
 int DamageCar3D(_CAR_DATA *cp, long(*delta)[4], int strikeVel, _CAR_DATA *pOtherCar)
 {
-	UNIMPLEMENTED();
-	return 0;
-	/*
 	char region;
 	int value;
 	int iVar1;
@@ -780,106 +778,130 @@ int DamageCar3D(_CAR_DATA *cp, long(*delta)[4], int strikeVel, _CAR_DATA *pOther
 	int iVar2;
 
 	iVar2 = strikeVel;
-	if (strikeVel < 0) {
-		iVar2 = strikeVel + 0x7f;
-	}
+	//if (strikeVel < 0)
+	//	iVar2 = strikeVel + 0x7f;
+
 	strikeVel = strikeVel * 0x177;
+
 	value = (uint)(ushort)(((cp->ap).carCos)->colBox).vz << 0x10;
 	value = (value >> 0x10) - (value >> 0x1f) >> 1;
-	if (strikeVel < 0) {
-		strikeVel = strikeVel + 0xff;
-	}
+
+	//if (strikeVel < 0)
+	//	strikeVel = strikeVel + 255;
+
 	strikeVel = strikeVel >> 8;
-	if (strikeVel < 0xa000) {
-		if (cp->totalDamage == 0) {
+
+	if (strikeVel < 0xa000) 
+	{
+		if (cp->totalDamage == 0)
 			cp->totalDamage = 1;
-		}
+
 		iVar2 = 0;
 	}
-	else {
-		iVar1 = (int)(cp->hd).where.m[2] * (*delta)[0] + (int)(cp->hd).where.m[5] * (*delta)[1] +
-			(int)(cp->hd).where.m[8] * (*delta)[2] + 0x800 >> 0xc;
-		if ((int)(cp->hd).where.m[0] * (*delta)[0] + (int)(cp->hd).where.m[3] * (*delta)[1] +
-			(int)(cp->hd).where.m[6] * (*delta)[2] + 0x800 >> 0xc < 1) {
-			region = '\0';
-			if ((iVar1 <= value) && (region = '\x04', -value < iVar1)) {
-				region = '\x05';
+	else 
+	{
+		iVar1 = (cp->hd.where.m[0][2] * (*delta)[0] + cp->hd.where.m[1][2] * (*delta)[1] + cp->hd.where.m[2][2] * (*delta)[2]) / 4096;
+
+		if ((cp->hd.where.m[0][0] * (*delta)[0] + cp->hd.where.m[1][0] * (*delta)[1] + cp->hd.where.m[2][0] * (*delta)[2]) / 4096 < 1)
+		{
+			region = 0;
+
+			if ((iVar1 <= value) && (region = 4, -value < iVar1)) 
+			{
+				region = 5;
 			}
 		}
-		else {
-			region = '\x01';
-			if ((iVar1 <= value) && (region = '\x03', -value < iVar1)) {
-				region = '\x02';
+		else 
+		{
+			region = 1;
+			if ((iVar1 <= value) && (region = 3, -value < iVar1))
+			{
+				region = 2;
 			}
 		}
-		if (cp->controlType == '\x01') {
-			value = (strikeVel / 0x15e + 0x200) * 3;
-			if (value < 0) {
-				value = value + 7;
-			}
+
+		if (cp->controlType == 1) 
+		{
+			value = (strikeVel / 350 + 0x200) * 3;
+			//if (value < 0)
+			//	value = value + 7;
+		
 			value = value >> 3;
-			if (0x477 < value) {
+			if (0x477 < value)
 				value = 0x477;
+		
+		}
+		else if (cp->controlType == 4)
+		{
+			if (pOtherCar->controlType == 1)
+			{
+				value = (strikeVel / 350 + 0x200) * 3;
+				//if (value < 0)
+				//	value = value + 7;
+
+				value = value >> 3;
+
+				if (0x477 < value)
+					value = 0x477;
+
+				cp->ai.l.takeDamage = 0x32;
+			}
+			else
+			{
+				value = strikeVel / 350 + 0x200;
+				//if (value < 0)
+				//	value = strikeVel / 350 + 0x203;
+
+				value = value >> 2;
+				if (0x2fa < value)
+					value = 0x2fa;
+
+				if (cp->ai.l.takeDamage == 0)
+					value = 0;
 			}
 		}
-		else {
-			if (cp->controlType == '\x04') {
-				if (pOtherCar->controlType == '\x01') {
-					value = (strikeVel / 0x15e + 0x200) * 3;
-					if (value < 0) {
-						value = value + 7;
-					}
-					value = value >> 3;
-					if (0x477 < value) {
-						value = 0x477;
-					}
-					cp->ai[100] = 0x32;
-				}
-				else {
-					value = strikeVel / 0x15e + 0x200;
-					if (value < 0) {
-						value = strikeVel / 0x15e + 0x203;
-					}
-					value = value >> 2;
-					if (0x2fa < value) {
-						value = 0x2fa;
-					}
-					if (cp->ai[100] == 0) {
-						value = 0;
-					}
-				}
-			}
-			else {
-				iVar1 = (strikeVel / 400 + 0x400) * 7;
-				value = iVar1 >> 3;
-				if (iVar1 < 0) {
-					value = iVar1 + 7 >> 3;
-				}
-			}
+		else 
+		{
+			iVar1 = (strikeVel / 400 + 0x400) * 7;
+			value = iVar1 >> 3;
+
+			//if (iVar1 < 0)
+			//	value = iVar1 + 7 >> 3;
 		}
+
 		fakeDamage = false;
-		if (cp->controlType == '\x03') {
-			fakeDamage = pOtherCar->controlType == '\x03';
-		}
+
+		if (cp->controlType == 3) 
+			fakeDamage = pOtherCar->controlType == 3;
+
 		ApplyDamage(cp, region, value, fakeDamage);
+
 		value = GetPlayerId(cp);
 		region = (char)value;
-		if (region < '\0') {
+
+		if (region < 0) 
+		{
 			value = GetPlayerId(pOtherCar);
 			region = (char)value;
 		}
+
 		value = GetPlayerId(cp);
-		if ((value != 0) || (value = 2, pOtherCar->controlType != '\x02')) {
+
+		if ((value != 0) || (value = 2, pOtherCar->controlType != 2)) 
+		{
 			iVar1 = GetPlayerId(pOtherCar);
 			value = 1;
-			if ((iVar1 == 0) && (cp->controlType == '\x02')) {
+
+			if ((iVar1 == 0) && (cp->controlType == 2)) 
 				value = 2;
-			}
 		}
+
 		CollisionSound(region, cp, iVar2 >> 7, value);
+
 		iVar2 = 1;
 	}
-	return iVar2;*/
+
+	return iVar2;
 }
 
 
