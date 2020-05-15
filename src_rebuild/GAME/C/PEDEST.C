5c0166ba6e3cc39aa422a0ce4d13f5aa0f4200fe
Ilya Shurumov <ishurumov@gmail.com>
2020-05-15 17:49:53 +0600

    - fix SetupGetInCar

--- src_rebuild/GAME/C/PEDEST.C
+++ src_rebuild/GAME/C/PEDEST.C
@@ -2488,7 +2488,7 @@ void SetupGetInCar(PEDESTRIAN *pPed)
 		player[0].cameraView = 5;
 		player[0].cameraPos.vx = carToGetIn->hd.where.t[0] - ((iVar5 * rcossin_tbl[uVar4 * 2 + 1] >> 0xc) - (rcossin_tbl[uVar4 * 2] * 800 >> 0xc));
 		player[0].cameraPos.vy = -200 - carToGetIn->hd.where.t[1];
-		player[0].cameraPos.vz = p_Var1->hd.where.t[2] + (iVar5 * rcossin_tbl[uVar4 * 2] >> 0xc) + (rcossin_tbl[uVar4 * 2 + 1] * 800 >> 0xc);
+		player[0].cameraPos.vz = carToGetIn->hd.where.t[2] + (iVar5 * rcossin_tbl[uVar4 * 2] >> 0xc) + (rcossin_tbl[uVar4 * 2 + 1] * 800 >> 0xc);
 	}
 
 	if ((carToGetIn->controlFlags & 4) == 0)
