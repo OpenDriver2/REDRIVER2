ddab3074c540aadfa9402caa8a7000946653eebe
Ilya Shurumov <ishurumov@gmail.com>
2020-09-20 17:25:43 +0600

    - zero flags in DrawTILES

--- src_rebuild/GAME/C/TILE.C
+++ src_rebuild/GAME/C/TILE.C
@@ -323,6 +323,7 @@ void DrawTILES(int tile_amount)
 	plotContext.ptexture_pages = (ushort(*)[128])texture_pages;
 	plotContext.ptexture_cluts = (ushort(*)[128][32])texture_cluts;
 	plotContext.lastTexInfo = 0x18273472;
+	plotContext.flags = 0;
 	ppuVar6 = (ushort **)tile_overflow_buffer;
 
 	while (tile_amount != -1) 
