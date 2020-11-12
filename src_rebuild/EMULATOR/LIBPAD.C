2b2ded5c0ab1b697215716bebf21abfab8fec79e
Ilya Shurumov <ishurumov@gmail.com>
2020-11-13 00:43:24 +0600

    - [EMU] activate pad analog state different way

--- src_rebuild/EMULATOR/LIBPAD.C
+++ src_rebuild/EMULATOR/LIBPAD.C
@@ -361,10 +361,10 @@ void InternalPadUpdates()
 				pad->status = 0;	// PadStateStable?
 
 				// switch to analog state
-				if(pad->analog[0] != 127 || 
-					pad->analog[1] != 127 || 
-					pad->analog[2] != 127 || 
-					pad->analog[3] != 127 || 
+				if(pad->analog[0] == 255 || 
+					pad->analog[1] == 255 || 
+					pad->analog[2] == 255 || 
+					pad->analog[3] == 255 || 
 					pad->id == 0x41)
 				{
 					eprintf("Switched controller type to ANALOG\n");
