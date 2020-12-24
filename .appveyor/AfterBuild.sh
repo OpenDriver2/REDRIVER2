29aeac35dd364d1c57bf95798295ece3aa322c07
SoapyMan <ishurumov@gmail.com>
2020-12-25 00:41:58 +0600

    - fix appveyor

--- .appveyor/AfterBuild.sh
+++ .appveyor/AfterBuild.sh
@@ -4,6 +4,6 @@ set -ex
 for config in debug release release_dev
 do
     cd "${APPVEYOR_BUILD_FOLDER}/src_rebuild/bin/${config^}"
-    cp -R $data_folder ./
+    cp -R ${APPVEYOR_BUILD_FOLDER}/data/* ./
     tar -czf "REDRIVER2_${config^}.tar.gz" *
 done
