e916698138186bc70693a0dcf9bbaac42ddbfca0
SoapyMan <ishurumov@gmail.com>
2020-12-25 00:34:36 +0600

    - linux copy files in AfterBuild.sh

--- .appveyor/AfterBuild.sh
+++ .appveyor/AfterBuild.sh
@@ -4,5 +4,6 @@ set -ex
 for config in debug release release_dev
 do
     cd "${APPVEYOR_BUILD_FOLDER}/src_rebuild/bin/${config^}"
+    cp -R $data_folder ./
     tar -czf "REDRIVER2_${config^}.tar.gz" *
 done
