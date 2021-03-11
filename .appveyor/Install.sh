529f306c9d430219d909bfaa4ce1c3e9a885f4b4
Ilya Shurumov <ishurumov@gmail.com>
2021-03-11 16:13:31 +0600

    - Install.sh fix

--- .appveyor/Install.sh
+++ .appveyor/Install.sh
@@ -8,9 +8,8 @@ cd "$APPVEYOR_BUILD_FOLDER/src_rebuild"
 curl "$linux_premake_url" -Lo premake5.tar.gz
 tar xvf premake5.tar.gz
 
-sudo apt-get install --no-install-recommends -y g++-7-multilib gcc-7-multilib
-
 sudo apt-get update -qq -y
+sudo apt-get install --no-install-recommends -y g++-7-multilib gcc-7-multilib
 sudo apt-get install -qq aptitude -y
 
 # fix Ubuntu's broken mess of packages using aptitude
