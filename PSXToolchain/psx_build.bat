e6c37a82523edcba646b5f272be173b855b7d738
50thomatoes50 <50thomatoes50@users.noreply.github.com>
2021-11-29 17:08:45 +0100

    psx_build : fix link failing if output folder doesn't exist

--- PSXToolchain/psx_build.bat
+++ PSXToolchain/psx_build.bat
@@ -7,6 +7,13 @@ rem Create a virtual drive
 SUBST X: %REDRIVER_FOLDER%\PSXToolchain
 
 set PATH=%PATH%;X:\mipsel-unknown-elf\bin;
+
+if exist %REDRIVER_FOLDER%\PSXToolchain\CDSrc\0_CD_DATA\ (
+  rem Yes
+) else (
+  mkdir %REDRIVER_FOLDER%\PSXToolchain\CDSrc\0_CD_DATA\
+)
+
 make
 
 rem Cleanup
