afe523d8e5e61f1c0fd55cdbe5651f0fb2f1ffdf
InspirationByte <ishurumov@gmail.com>
2022-01-20 21:28:17 +0300

    - PsyCross is no longer a part of ReDriver2 and separated into Submodule

--- src_rebuild/PsyCross/include/psx/rand.h
+++
@@ -1,7 +0,0 @@
-#ifndef RAND_H
-#define RAND_H
-
-#include <stdint.h>
-#include <stdlib.h>
-
-#endif
--- src_rebuild/PsyCross/src/psx/ABS.C
+++
@@ -1,8 +0,0 @@
-#include "psx/abs.h"
-
-// this is definitely NOT in psx runtime libs
-int fst_abs(int x)
-{
-	const int mask = x >> 31;
-	return (x ^ mask) - mask;
-}
