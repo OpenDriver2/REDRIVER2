4e30d85dc0c0d696e1ea9830f508086d28d40a29
SoapyMan <ishurumov@gmail.com>
2025-03-14 01:41:55 +0500

    - add intptr, u_intptr typedefs

--- src_rebuild/Game/psyx_compat.h
+++ src_rebuild/Game/psyx_compat.h
@@ -21,6 +21,10 @@ typedef short VERTTYPE;
 
 #define RECT16 RECT
 
+// for portability, mainly 64 bit platforms
+typedef int			intptr;
+typedef u_int		u_intptr;
+
 #else
 
 #if defined(_M_X64) || defined(__amd64__)
@@ -28,6 +32,10 @@ typedef OT_TAG OTTYPE;
 #else
 typedef unsigned long long OTTYPE;
 #endif
+
+// for portability, mainly 64 bit platforms
+typedef intptr_t	intptr;
+typedef uintptr_t	u_intptr;
 
 #endif
 
