d3daf3c250415bd9b958cc4b55f008bf1870c5e6
SoapyMan <ishurumov@gmail.com>
2025-04-03 20:57:33 +0500

    - bump version to 8.0

--- src_rebuild/Game/version.h
+++ src_rebuild/Game/version.h
@@ -1,9 +1,9 @@
 #ifndef GAME_VERSION_N
-#define GAME_VERSION_N		"7.8"
+#define GAME_VERSION_N		"8.0"
 #endif
 
 #ifndef GAME_VERSION_RES
-#define GAME_VERSION_RES	7,8
+#define GAME_VERSION_RES	8,0
 #endif
 
 #define GAME_TITLE			"REDRIVER2"
